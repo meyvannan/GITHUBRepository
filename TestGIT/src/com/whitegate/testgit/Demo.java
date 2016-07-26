632b05d15d7a4028bced6a1bf3b6091347a64934
meyvannan <meyvannan@gmail.com>
2016-07-26 11:29:29 +0530

    3rd commit

--- TestGIT/src/com/whitegate/testgit/Demo.java
+++ TestGIT/src/com/whitegate/testgit/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		System.out.println("This is the first GITHUB Configuration program");
 		System.out.println("This is the first GITHUB Configuration program-2nd Commit");
+		System.out.println("This is the first GITHUB Configuration program-2nd Commit");
 	}
 
 }
